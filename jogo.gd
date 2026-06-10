9dc0de2f12f073ba98e87dc968536f57751cd5d4
KahhSilvaa <karolina.ssilvaa@gmail.com>
2026-06-10 17:16:46 -0300

    jogador

--- jogo.gd
+++ jogo.gd
@@ -6,15 +6,17 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	$fundo.position.x   -= velocidade*delta*0.05
 	$fundo2.position.x  -= velocidade*delta*0.05
-	if $fundo.position.x < -1152: # se o fundo sair de cena pela esquerda
-		$fundo.position.x = 1152 # coloque ele após a cena na direita
+	if $fundo.position.x < -1152: 
+		$fundo.position.x = 1152 
 	if $fundo2.position.x < -1152:
 		$fundo2.position.x = 1152
 
-	# constantemente desloca o sprite pra esquerda
-	$chao.position.x -= velocidade*delta*2
-	$chao2.position.x -= velocidade*delta*2
-	if $chao.position.x < -1152: # se o fundo sair de cena pela esquerda
-		$chao.position.x = 1152 # coloque ele após a cena na direita
+
+	$chao.position.x -= velocidade*delta*1.5
+	$chao2.position.x -= velocidade*delta*1.5
+	if $chao.position.x < -1152: 
+		$chao.position.x = 1152 
 	if $chao2.position.x < -1152:
 		$chao2.position.x = 1152
+		
+		
